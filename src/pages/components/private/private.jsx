e66849d90201efce6c8f798f2379a222f7a2ed22
dt2g23 <dt2g23@soton.ac.uk>
2024-07-29 16:53:59 +0100

    Additional changes using react-toastify added

--- src/pages/components/private/private.jsx
+++ src/pages/components/private/private.jsx
@@ -4,6 +4,7 @@ import './private.css';
 import { useState } from "react";
 import axios from 'axios';
 import { Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export const Private = () => {
 
@@ -27,8 +28,10 @@ export const Private = () => {
             navigate('/patient', {state: {patientInfo, scanInfo}});
             setPatientInfo(patientInfo);
             setScanID(scanInfo);
+            toast.success("Patient ID successfully found")
         } catch (error) {
             console.error('Error fetching the patient data', error);
+            toast.error("Patient ID is wrong, try again!")
         }
     };
 
@@ -42,7 +45,10 @@ export const Private = () => {
 
     const handleSignOut = () => {
         signOut(auth)
-        .then(() => console.log("Sign Out"))
+        .then(() => {
+            console.log("Sign Out")
+            toast.success("Successfully signed out!")
+        })
         .catch((error) => console.log(error))
     }
 
@@ -52,7 +58,7 @@ export const Private = () => {
                 <img src="src\pictures\dotplot.jpeg" />
             </div>
 
-            <h2>Patient Name / Patient ID</h2>
+            <h2>Patient ID</h2>
             <div id="bottom">
                 <input type="number" placeholder="Search..." value={input} onChange={handleInputChange} />
             
